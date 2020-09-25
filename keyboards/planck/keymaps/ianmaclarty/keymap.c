005381a9967ce3094092f16d063d12584275bd4e
Ian MacLarty <ian@ianmaclarty.com>
2020-09-25 15:38:03 +1000

    disable enter on other layers to prevent accidental presses

--- keyboards/planck/keymaps/ianmaclarty/keymap.c
+++ keyboards/planck/keymaps/ianmaclarty/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_CAPS] = LAYOUT_planck_grid(
         TRNS,       RSFT(KC_Q), RSFT(KC_W), RSFT(KC_E), RSFT(KC_R), RSFT(KC_T), LSFT(KC_Y), LSFT(KC_U), LSFT(KC_I),     LSFT(KC_O),     LSFT(KC_P),     TRNS,
         KC_TILD,    RSFT(KC_A), RSFT(KC_S), RSFT(KC_D), RSFT(KC_F), RSFT(KC_G), LSFT(KC_H), LSFT(KC_J),  LSFT(KC_K),    LSFT(KC_L),     KC_COLN,        KC_DQUO,
-        TRNS,       RSFT(KC_Z), RSFT(KC_X), RSFT(KC_C), RSFT(KC_V), RSFT(KC_B), LSFT(KC_N), LSFT(KC_M), KC_LABK,        KC_RABK,        KC_QUES,        LSFT(KC_ENTER),
+        TRNS,       RSFT(KC_Z), RSFT(KC_X), RSFT(KC_C), RSFT(KC_V), RSFT(KC_B), LSFT(KC_N), LSFT(KC_M), KC_LABK,        KC_RABK,        KC_QUES,        KC_NO,
         TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       KC_NO,      TRNS,       TRNS,           TRNS,           TRNS,           TRNS
     ),
 
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUMBERS] = LAYOUT_planck_grid(
         KC_DELETE,  KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       TRNS,
         KC_GRAVE,   KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,    KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_MINUS,   KC_EQUAL,   KC_PIPE,
-        TRNS,       KC_LCBR,    KC_RCBR,    KC_LBRACKET,KC_RBRACKET,KC_BSLASH,  KC_SLASH,   KC_LPRN,    KC_RPRN,    KC_UNDS,    KC_PLUS,    TRNS,
+        TRNS,       KC_LCBR,    KC_RCBR,    KC_LBRACKET,KC_RBRACKET,KC_BSLASH,  KC_SLASH,   KC_LPRN,    KC_RPRN,    KC_UNDS,    KC_PLUS,    KC_NO,
         TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       KC_NO,      TRNS,       TRNS,       KC_HOME,    TRNS,       KC_END
     ),
 
