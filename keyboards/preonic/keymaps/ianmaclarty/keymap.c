1a9c4fabbc478e4db4d7f748bb8489dae1e63c50
Ian MacLarty <ian@ianmaclarty.com>
2020-12-28 16:23:50 +1100

    new preonic keymap

--- keyboards/preonic/keymaps/ianmaclarty/keymap.c
+++ keyboards/preonic/keymaps/ianmaclarty/keymap.c
@@ -52,12 +52,12 @@ enum preonic_layers {
 #define PURPLE {128, 0, 255}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_BASE] = LAYOUT_preonic_grid( \
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
-  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT,
-  LT_FN,   KC_LALT, KC_LGUI, LT_NUM,  LT_NUM,  KC_SPC,  KC_SPC,  LT_CAPS, LT_CAPS, KC_LEFT, KC_DOWN, KC_RIGHT
+[_BASE] = LAYOUT_preonic_grid( 
+  KC_GRAVE,KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
+  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLASH,
+  KC_LCTL, KC_LGUI, KC_LALT, LT_FN,   LT_NUM,  KC_SPC,  KC_SPC,  KC_RSFT, KC_NO,   KC_LEFT, KC_DOWN, KC_RIGHT
 ),
 
 [_CAPS] = LAYOUT_preonic_grid(
@@ -69,8 +69,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_FUNCTION] = LAYOUT_preonic_grid(
-    TG(_RESET), TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       KC_KP_7,    KC_KP_8,    KC_KP_9,    TRNS,       KC_PGUP,
-    TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       KC_KP_4,    KC_KP_5,    KC_KP_6,    KC_PSCREEN, KC_PGDOWN,
+    TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       KC_KP_7,    KC_KP_8,    KC_KP_9,    TRNS,       KC_PGUP,
+    TG(_RESET), TRNS,       TRNS,       TRNS,       RGB_TOG,    TRNS,       TRNS,       KC_KP_4,    KC_KP_5,    KC_KP_6,    KC_PSCREEN, KC_PGDOWN,
     TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       KC_KP_1,    KC_KP_2,    KC_KP_3,    TRNS,       TRNS,
     TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       KC_KP_0,    TRNS,       TRNS,       TRNS,       TRNS,
     TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       VOLDOWN,    VOLMUTE,    VOLUP
@@ -78,9 +78,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_NUMBERS] = LAYOUT_preonic_grid(
     KC_F12,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,    
-    KC_GRAVE,   KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_PIPE,
+    KC_DELETE,  KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_PIPE,
     TRNS,       KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,    KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_MINUS,   KC_EQUAL,   TRNS,
-    TRNS,       KC_LCBR,    KC_RCBR,    KC_LBRACKET,KC_RBRACKET,KC_BSLASH,  KC_SLASH,   KC_LPRN,    KC_RPRN,    KC_UNDS,    KC_PLUS,    TRNS,
+    TRNS,       KC_LCBR,    KC_RCBR,    KC_LBRACKET,KC_RBRACKET,TRNS,       TRNS,       KC_LPRN,    KC_RPRN,    KC_UNDS,    KC_PLUS,    KC_BSLASH,
     TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       TRNS,       KC_NO,      TRNS,       TRNS,       KC_HOME,    TRNS,       KC_END
 ),
 
