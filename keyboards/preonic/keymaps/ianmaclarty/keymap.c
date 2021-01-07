050b23fcdc0405640a21c3391df708a8af54ccc2
Ian MacLarty <ian@ianmaclarty.com>
2021-01-08 10:52:38 +1100

    move fn key

--- keyboards/preonic/keymaps/ianmaclarty/keymap.c
+++ keyboards/preonic/keymaps/ianmaclarty/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE,   KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_SLASH,
     KC_TAB,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
     KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_UP,      KC_RSFT,
-    KC_LCTL,    KC_LGUI,    KC_LALT,    KC_SPC,     MO(_FN),    MO(_FN),    KC_SPC,     KC_SPC,     KC_ENT,     KC_LEFT,    KC_DOWN,    KC_RIGHT
+    KC_LCTL,    MO(_FN),    KC_LALT,    KC_LGUI,    KC_SPC,     KC_SPC,     KC_SPC,     KC_SPC,     KC_ENT,     KC_LEFT,    KC_DOWN,    KC_RIGHT
 ),
 
 [_FN] = LAYOUT_preonic_grid(
