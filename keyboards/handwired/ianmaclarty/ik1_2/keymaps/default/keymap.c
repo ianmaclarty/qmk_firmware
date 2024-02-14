74a061f518e4918f8432597c8cf4685ea301989d
Ian MacLarty <ian.maclarty@gmail.com>
2024-02-14 11:48:40 +1100

    move base layer select to its own layer

--- keyboards/handwired/ianmaclarty/ik1_2/keymaps/default/keymap.c
+++ keyboards/handwired/ianmaclarty/ik1_2/keymaps/default/keymap.c
@@ -1,10 +1,11 @@
 #include QMK_KEYBOARD_H
 
-#define LBASE 0
+#define LBASE1 0
 #define LBASE2 1
 #define LNUM 2
 #define LNAV 3
 #define LFN 4
+#define LBSEL 5
 
 #define TABNUM LT(LNUM, KC_TAB)
 #define ENTNAV LT(LNAV, KC_ENT)
@@ -12,12 +13,13 @@
 #define NAV MO(LNAV)
 #define BSSFT MT(MOD_RSFT,KC_BSPC)
 #define FN MO(LFN)
-#define BASE1 DF(LBASE)
+#define BASE1 DF(LBASE1)
 #define BASE2 DF(LBASE2)
+#define BSEL MO(LBSEL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[LBASE] = LAYOUT_split_4x6_3(
+[LBASE1] = LAYOUT_split_4x6_3(
 _______,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                              KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       _______,
 KC_ESC,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                              KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_DEL,
 KC_LALT,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                              KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
@@ -43,8 +45,8 @@ _______,    _______,    _______,    _______,    _______,    _______,            
 
 [LNAV] = LAYOUT_split_4x6_3(
 _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                              KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     _______,
-BASE2,      KC_UNDS,    KC_PLUS,    KC_LCBR,    KC_RCBR,    KC_TILD,                            KC_PGUP,    KC_HOME,    KC_UP,      KC_END,     KC_PSCR,    KC_BSPC,
-BASE1,      KC_MINS,    KC_EQL,     KC_LBRC,    KC_RBRC,    KC_GRV,                             KC_PGDN,    KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_PIPE,    _______,
+_______,    KC_UNDS,    KC_PLUS,    KC_LCBR,    KC_RCBR,    KC_TILD,                            KC_PGUP,    KC_HOME,    KC_UP,      KC_END,     KC_PSCR,    KC_BSPC,
+_______,    KC_MINS,    KC_EQL,     KC_LBRC,    KC_RBRC,    KC_GRV,                             KC_PGDN,    KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_PIPE,    _______,
 _______,    KC_F11,     KC_F12,     KC_LPRN,    KC_RPRN,    _______,                            KC_INS,     KC_MUTE,    KC_VOLD,    KC_VOLU,    KC_BSLS,    _______,
                                                 _______,    _______,    _______,    _______,    _______,    _______
 ),
@@ -53,7 +55,15 @@ _______,    KC_F11,     KC_F12,     KC_LPRN,    KC_RPRN,    _______,            
 _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                              KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     _______,
 _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      _______,                            _______,    _______,    _______,    _______,    KC_PSCR,    _______,
 _______,    KC_F5,      KC_F6,      KC_F7,      KC_F8,      _______,                            _______,    _______,    _______,    _______,    _______,    _______,
-_______,    KC_F9,      KC_F10,     KC_F11,     KC_F12,     _______,                            _______,    _______,    _______,    _______,    _______,    _______,
+_______,    KC_F9,      KC_F10,     KC_F11,     KC_F12,     BSEL,                               _______,    _______,    _______,    _______,    _______,    _______,
+                                                _______,    _______,    _______,    _______,    _______,    _______
+),
+
+[LBSEL] = LAYOUT_split_4x6_3(
+_______,    BASE1,      BASE2,      _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    _______,
+_______,    _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    _______,
+_______,    _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    _______,
+_______,    _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    _______,
                                                 _______,    _______,    _______,    _______,    _______,    _______
 ),
                              
