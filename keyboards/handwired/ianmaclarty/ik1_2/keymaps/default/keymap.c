f0840b13e4e20f636765c2a414a6bde12ff48f48
Ian MacLarty <ian.maclarty@gmail.com>
2024-09-25 17:28:32 +1000

    keymap tweaks

--- keyboards/handwired/ianmaclarty/ik1_2/keymaps/default/keymap.c
+++ keyboards/handwired/ianmaclarty/ik1_2/keymaps/default/keymap.c
@@ -9,6 +9,7 @@
 
 #define TABNUM LT(LNUM, KC_TAB)
 #define ENTNAV LT(LNAV, KC_ENT)
+#define ENTFN LT(LFN, KC_ENT)
 #define BSNAV LT(LNAV, KC_BSPC)
 #define NAV MO(LNAV)
 #define BSSFT MT(MOD_RSFT,KC_BSPC)
@@ -24,30 +25,30 @@ _______,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,               
 KC_ESC,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                              KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_DEL,
 KC_LALT,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                              KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
 KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                              KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RGUI,
-                                                KC_LCTL,    TABNUM,     KC_SPC,    BSSFT,      ENTNAV,     FN
+                                                KC_LCTL,    TABNUM,     KC_SPC,    BSSFT,      NAV,        ENTFN
 ),
 
 [LBASE2] = LAYOUT_split_4x6_3(
 _______,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                              KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       _______,
 KC_ESC,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                              KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_DEL,
-KC_TAB,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                              KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
+TABNUM,     KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                              KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,
 KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,                              KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RGUI,
-                                                KC_LALT,    KC_LCTL,    KC_SPC,    BSSFT,      ENTNAV,     FN
+                                                KC_LALT,    KC_LCTL,    KC_SPC,    BSSFT,      NAV,        ENTFN
 ),
 
 [LNUM] = LAYOUT_split_4x6_3(
 _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                              KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     _______,
 _______,    KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_TILD,                            _______,    KC_7,       KC_8,       KC_9,       _______,    KC_BSPC,
 _______,    KC_PERC,    KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_GRV,                             KC_MINS,    KC_4,       KC_5,       KC_6,       KC_PIPE,    _______,
-_______,    _______,    _______,    _______,    _______,    _______,                            _______,    KC_1,       KC_2,       KC_3,       KC_BSLS,    _______,
-                                                _______,    _______,    _______,    _______,    KC_0,       KC_DOT
+_______,    _______,    _______,    _______,    _______,    _______,                            KC_DOT,     KC_1,       KC_2,       KC_3,       KC_BSLS,    _______,
+                                                _______,    _______,    _______,    _______,    KC_0,       _______
 ),
 
 [LNAV] = LAYOUT_split_4x6_3(
 _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,                              KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     _______,
 _______,    KC_UNDS,    KC_PLUS,    KC_LCBR,    KC_RCBR,    KC_TILD,                            KC_PGUP,    KC_HOME,    KC_UP,      KC_END,     KC_PSCR,    KC_BSPC,
-_______,    KC_MINS,    KC_EQL,     KC_LBRC,    KC_RBRC,    KC_GRV,                             KC_PGDN,    KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_PIPE,    _______,
-_______,    KC_F11,     KC_F12,     KC_LPRN,    KC_RPRN,    _______,                            KC_INS,     KC_MUTE,    KC_VOLD,    KC_VOLU,    KC_BSLS,    _______,
+_______,    KC_MINS,    KC_EQL,     KC_LBRC,    KC_RBRC,    KC_GRV,                             KC_PGDN,    KC_LEFT,    KC_DOWN,    KC_RIGHT,   _______,    _______,
+_______,    KC_UNDS,    KC_PLUS,    KC_LPRN,    KC_RPRN,    _______,                            KC_INS,     KC_MUTE,    KC_VOLD,    KC_VOLU,    _______,    _______,
                                                 _______,    _______,    _______,    _______,    _______,    _______
 ),
                              
